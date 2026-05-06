88b12524f4751de3c3e48ca3132f482092a74f7f
Pietro Pansani <pietropansani038@gmail.com>
2026-05-06 11:25:08 -0300

    meu primeiro programa python

---
+++ 1.1-numeros-em-python.py
@@ -0,0 +1,25 @@
+# AULA COMPLETA: NUMEROS EM PYTHON
+
+"""
+O que vamos aprender:
+1 - Tipos numericos
+2 - Conversões de tipos
+3 - Hierarquia númerica
+4 - Operações matematicas
+5 - Coerção de tipos
+6 - Verificação de tipos
+7 - Entrada de Dados
+"""
+
+print("==== TIPOS NÚMERICOS ====")
+
+# EXEMPLO 01 - NUMERO INTEIRO
+
+# criamos uma variavel chamada numero_inteiro
+numero_inteiro = 10
+# mostrar o valor
+print("Valor:", numero_inteiro)
+#Type() mostra qual é o tipo da variável
+print("Tipo:", type(numero_inteiro))
+
+print("--------------------------------")
