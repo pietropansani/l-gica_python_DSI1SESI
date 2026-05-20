90b210ee5fc1d9443ad1d2f40c8a9ce6f14f5b95
Pietro Pansani <pietropansani038@gmail.com>
2026-05-20 08:08:52 -0300

    Exercicios Python

---
+++ exercicios.py
@@ -0,0 +1,159 @@
+# Exercicio 1
+print("Exercicio 1:")
+print("\n")
+
+# Um aluno tem 10 anos. Armazene essa idade em uma variável
+idade = 10
+
+print("Um aluno tem 10 anos. Armazene essa idade em uma variavel")
+print("idade:", idade)
+
+# Divisor
+print("==========================================================")
+
+# Exiba seu tipo
+print("Tipo:", type(idade))
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 2
+print("Exercicio 2:")
+print("\n")
+# A temperatura medida é 23.5°C.
+temperatura = 23.5
+
+print("Temperatura Atual:", temperatura)
+
+# Divisor
+print("==========================================================")
+
+# Armazene esse valor e mostre seu tipo.
+print("Tipo:", type(temperatura))
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 3
+print("Exercicio 3:")
+print("\n")
+
+# Crie um número complexo representando uma impedância elétrica
+impedancia = 5 + 8j
+
+print("Impedãncia / Parte Real:", impedancia.real)
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 4
+print("Exercicio 4:")
+print("\n")
+
+# Mostre a parte imaginária do número complexo criado no exercício anterior.
+print("Impedância / Parte Imaginária :", impedancia.imag)
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 5
+print("Exercicio 5:")
+print("\n")
+
+# Declare uma variável chamada "populacao" com o valor 8_000_000_000 (8 bilhões) e mostre seu tipo.
+populacao = 8_000_000_000
+
+# Mostrando tipo:
+print("Tipo:", type(populacao))
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 6
+print("Exercicio 6:")
+print("\n")
+
+# Verifique se o número 7 é do tipo int usando a função type().
+n = 7
+
+# Verificando se o número 7 é do tipo int
+print("Verificando:", type(n))
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 7
+print("Exercicio 7:")
+print("\n")
+
+# Variavel booleana
+aprovado = True
+
+# Mostrando o tipo dela:
+print("Tipo da Variavel:", aprovado)
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 8
+print("Exercicio 8:")
+print("\n")
+
+# Variaveis
+
+soma = True + False
+resultado = soma
+
+# Print
+
+print("O Resultado foi:", resultado)
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 9
+print("Exercicio 9:")
+print("\n")
+
+import sys  # noqa: E402
+
+# NAO EXISTE LIMITE FIXO PROS NUMEROS INTEIROS NO PYTHON 3
+print("No Python, inteiros nao tem um limite fixo oficial, porem posso citar um exemplo de um numero extremamente grande")
+
+numero_enorme = 10**1000
+
+print("Um exemplo de um numero enorme:", numero_enorme)
+
+# PEDI AJUDA DO GEMINI E ELE ME DEU ISSO:
+
+print("O valor de sys.maxsize nesta maquina e:", sys.maxsize)
+
+# Divisor
+print("==========================================================")
+
+# Exercicio 10
+print("Exercicio 10:")
+print("\n")
+
+# Divisor
+print("==========================================================")
+
+# Mostre a representação em binário do número 10 usando uma função do Python.
+
+# Definindo um Numero
+numero = 10
+
+# Divisor
+print("==========================================================")
+
+# Funcao bin() para converter
+ref_binaria = bin(numero)
+
+# Divisor
+print("==========================================================")
+
+# Mostrando o Resultado
+print(f"A representacao em binario do numero: {numero} é {ref_binaria}")
+
+# Divisor
+print("==========================================================")
